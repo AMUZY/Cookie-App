eed470f46d665310f810aa68e7ca2abcced5c092
amuzy <nnamdiamu5@gmail.com>
2023-02-25 21:26:34 +0100

    Pic Size reduced

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,15 @@
-import { useLocation } from "react-router-dom"
 import "../App.scss"
+import "../about.scss"
 import MyFooter from "../components/MyFooter"
 
 
-
 export default function About () {
-    let link = useLocation();
-    if(link.pathname === "/about"){
-        alert("ABOUT PAGE ISN'T FINISHED YET")
-      }
     return (
         <div>
+            {/* ABOUT US DIV */}
+            <div className="my-8 flex justify-center items-center md:my-16">
+                <h2 className="script text-black text-3xl font-bold md:text-5xl xl:text-7xl animate">Recipes</h2>
+            </div>
             <MyFooter />
         </div>
   
